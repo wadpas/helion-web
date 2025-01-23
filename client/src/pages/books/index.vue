f5d28f03cd647a87aca36015b6f4858e6b728d5d
wadpas <wadpas@gmail.com>
2025-01-23 20:45:38 +0200

    add base files

--- client/src/pages/books/index.vue
+++
@@ -1,8 +0,0 @@
-<template>
-  <div>Books</div>
-  <router-link to="/">Home</router-link>
-</template>
-
-<script setup lang="ts"></script>
-
-<style scoped></style>
--- client/src/pages/index.vue
+++
@@ -1,8 +0,0 @@
-<template>
-  <div>Home</div>
-  <router-link :to="{ name: '/books/[id]', params: { id: 1 } }">Books 1</router-link>
-</template>
-
-<script setup lang="ts"></script>
-
-<style scoped></style>
